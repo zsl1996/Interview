64127f9114ee788f9443bb39e65a7d4003046310
zsl <31581150+zsl1996@users.noreply.github.com>
2018-12-19 21:48:59 +0800

    add a new solution

--- two sorted array find.cpp
+++ two sorted array find.cpp
@@ -31,9 +31,39 @@ double find_midsortedarrays(vector<int>& v1,vector<int>& v2,int k){
 		 else break;
 	}
 	return (std::max(L1,L2)+std::min(R1,R2))/2.0;
-
-
-
 }
 
 
+
+
+
+
+//bymyself
+double find_midsortedarrays(vector<int>& v1,vector<int>& v2,int k){
+
+	int L1 = v1.size();
+	int L2 = v2.size();
+	if(L1 > L2) return find_midsortedarrays(v2,v1,k);
+	int l = 0;
+	int r = L1 - 1;
+	int p1 = 0;
+	int p2 = 0;
+	while(l <= r){
+		p1 = (l + r)/2;  //p1 + 1 elements actually
+		p2 = k - p1 - 1;   //k - p1 elements,p2 is not in k
+		if (v1[p1] < v2[p2]) 
+		{
+			l = p1 + 1;
+		}
+		else if (v1[p1] == v2[p2])
+		{
+			return v1[p1];
+		}
+		else{
+			r = p1 - 1;
+		}
+	}
+	if(r == -1) return v2[k-1];
+	if(l == L1) return std::min(v1[L1-1],v2[k-L1-1]);
+	return std::min(v1[p1],v2[k-p1-1]);
+}
