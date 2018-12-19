5551773c2e22716a3d0e24b592ab35a5166a8456
zsl <31581150+zsl1996@users.noreply.github.com>
2018-12-19 20:44:59 +0800

    two sorted array find kth min number

--- two sorted array find.cpp
+++ two sorted array find.cpp
@@ -1,47 +1,39 @@
+// trick
 //two sorted array find kth min number
-
+// add # make always odd !!
+//refer to https://www.cnblogs.com/voidsky/p/5373982.html
 using namespace std;
-int k_min(vector<int> v1,vector<int> v2, int k){
-	int l1 = v1.size();
-	int l2 = v2.size();
-	if (l1 +l2 < k)
+
+double find_midsortedarrays(vector<int>& v1,vector<int>& v2,int k){
+	int n = v1.size();
+	int m = v2.size();
+	if (n > m)  //make sure V1 is  shorted
 	{
-		return -1;
+		return find_midsortedarrays(v2,v1);
 	}
-	int t1 = 0;
-	int t2 = 0;
-	int step = 0;
-	while(t1 + t2 != k-1 ){
-		step = (k - (t1 + t2) )/2；
-		if (t1 + step <= l1 && t2 + step <= l2)
-		{
-			if (v1[t1 + step -1] > v2[t2 + step -1])
-			{
-				t2 = t2 +step;
-			}
-			else{
-				t1 = t1 + step;
-			}
-		}
-		else if (t1 + step > l1)
-		{
-			if (v1[l1 -1] > v2[t2 + step -1])
-			{
-				t2 = t2 +step;
-			}
-			else{
-				t1 = l1 -1;
-			}
-		else{
-			if (v1[t1 +step -1] > v2[l2 -1])
-			{
-				t1 = t1 +step;
-			}
-			else{
-				t2 = l2 -1;
-			}
-		}		
-		}
+	int L1,L2,R1,R2,c1,c2,lo = 0,hi = 2*n;
+	while（lo <= hi）{
+		c1 = (lo+hi)/2;
+		c2 = 2*k-c1;
+		L1 = (c1 == 0)? INT32_MIN : v1[(c1-1)/2]; // map to original element
+		R1 = (c1 == 2*n)? INT32_MAX : v2[c1/2];
+		L2 = (c2 == 2*m)? INT32_MIN : v2[(c2-1)/2];
+		R2 = (c2 == 2*m)? INT32_MAX : v2[c2/2];
+
+		if (L1 > R2)
+		 {
+		 	hi = c1-1;
+		 } 
+		 else if (L2 > R1)
+		 {
+		 	lo = c1+1;
+		 }
+		 else break;
 	}
-	return std::min(v1[t1],v2[t2]);
+	return (std::max(L1,L2)+std::min(R1,R2))/2.0;
+
+
+
 }
+
+
